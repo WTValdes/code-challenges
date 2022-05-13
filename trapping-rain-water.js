2a52d3c5d7d4ceff5426422d31d4928f3b6d69bc
William Valdes <wtvaldes@gmail.com>
2022-05-12 17:35:46 -0700

    Add files via upload

---
+++ trapping-rain-water.js
@@ -0,0 +1,45 @@
+/*
+
+42. Trapping Rain Water
+
+Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+ 
+
+Example 1:
+
+
+Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
+Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+Example 2:
+
+Input: height = [4,2,0,3,2,5]
+Output: 9
+ 
+
+Constraints:
+
+n == height.length
+1 <= n <= 2 * 104
+0 <= height[i] <= 105
+
+ * @param {number[]} height
+ * @return {number}
+ */
+
+ function trap(height) {
+    let res = 0;
+    const len = height.length;
+    for (let i = 1; i < len - 1; i++) {
+      let maxLeft = 0, maxRight = 0;
+      for (let j = i; j >= 0; j--) {
+        maxLeft = Math.max(maxLeft, height[j]);
+      }
+      for (let j = i; j < len; j++) {
+        maxRight = Math.max(maxRight, height[j]);
+      }
+      res += Math.min(maxLeft, maxRight) - height[i];
+    }
+    return res;
+  };
