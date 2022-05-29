6d936af7d9d1b00360410e40429d729a3c1d4f30
William Valdes <wtvaldes@gmail.com>
2022-05-28 23:27:45 -0700

    Add files via upload

---
+++ is-tandem-repeat.js
@@ -0,0 +1,29 @@
+/*
+Determine whether the given string can be obtained by one concatenation of some string to itself.
+
+Example
+
+For inputString = "tandemtandem", the output should be
+solution(inputString) = true;
+For inputString = "qqq", the output should be
+solution(inputString) = false;
+For inputString = "2w2ww", the output should be
+solution(inputString) = false.
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[input] string inputString
+
+Guaranteed constraints:
+2 ≤ inputString.length ≤ 20.
+
+[output] boolean
+
+true if inputString represents a string concatenated to itself, false otherwise.
+*/
+
+function solution(inputString) {
+    return inputString.slice(0, inputString.length / 2) === 
+    inputString.slice(inputString.length / 2, inputString.length);
+}
