fc1aac6502842fcf4589a259bcc97826c9b39476
WTValdes <wtvaldes@gmail.com>
2022-07-02 16:39:51 -0700

    alternateCapitalization.js

---
+++ alternateCapitalization.js
@@ -0,0 +1,17 @@
+/*
+Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even.
+
+For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
+
+The input will be a lowercase string with no spaces.
+
+Good luck!
+
+
+*/
+
+function capitalize(s){
+    return [s.split('').map((a, i) => i % 2 === 0 ? a = a.toUpperCase() : a).join(''), 
+            s.split('').map((a, i) => i % 2 !== 0 ? a = a.toUpperCase() : a).join('')];
+  };
+
