44219725733c6688e6394a5c92fbe8b23a9e40be
WTValdes <wtvaldes@gmail.com>
2022-07-17 15:06:41 -0700

    gracefulTipping.js

---
+++ gracefulTipping.js
@@ -0,0 +1,33 @@
+/*
+Adding tip to a restaurant bill in a graceful way can be tricky, thats why you need make a function for it.
+
+The function will receive the restaurant bill (always a positive number) as an argument. You need to 1) add at least 15% in tip, 2) round that number up to an elegant value and 3) return it.
+
+What is an elegant number? It depends on the magnitude of the number to be rounded. Numbers below 10 should simply be rounded to whole numbers. Numbers 10 and above should be rounded like this:
+
+10 - 99.99... ---> Round to number divisible by 5
+
+100 - 999.99... ---> Round to number divisible by 50
+
+1000 - 9999.99... ---> Round to number divisible by 500
+
+And so on...
+
+Good luck!
+*/
+
+function gracefulTipping(bill) {
+    bill = bill * 1.15;
+    
+    if (bill < 10) {
+      return Math.ceil(bill)
+     }
+    
+    let div = parseInt('5' + '0'.repeat(Math.floor(bill).toString().length - 2));
+    
+    if (bill % div !== 0) {
+      bill = Math.ceil(bill / div) * div;
+    }
+    return bill;
+  }
+
