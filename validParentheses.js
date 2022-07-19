e413ef29edb50ef842b55148b6981e9e22c39be2
WTValdes <wtvaldes@gmail.com>
2022-07-19 13:45:58 -0700

    solution tweak

--- validParentheses.js
+++ validParentheses.js
@@ -1,14 +1,3 @@
-/*
-Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
-
-Examples
-"()"              =>  true
-")(()))"          =>  false
-"("               =>  false
-"(())((()())())"  =>  true
-
-*/
-
 function validParentheses(parens) {
     parens = parens.split('');
     let open = 0;
@@ -25,10 +14,5 @@ function validParentheses(parens) {
         return false;
       }
     }
-    
-    let leftCount = parens.filter(a => a === "(").length;
-    let rightCount = parens.filter(b => b === ")").length;
-    
-    return leftCount === rightCount;
+    return open === close;
   }
-
